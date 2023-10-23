79ceec2a4c5590653ed5f4b6bf9b2de1725f0ebf
Михаил <misha.odegoff@yandex.ru>
2023-10-23 20:05:40 +0500

    err

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   theme: {
     colors: {
       white: 'white',
